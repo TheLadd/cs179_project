db99570eb7833b791d940d669fc616c224e0d2a9
Jordell <rjordell@gmail.com>
2023-12-15 22:47:09 -0800

    cleand

--- r3/server/app.py
+++ r3/server/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from CargoState import Container, CargoState
-from Tree import Node, Tree
-from search import astar, backtrace, readManifest
 
 app = Flask(__name__)
 CORS(app)
