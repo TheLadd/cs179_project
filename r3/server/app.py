6ce368e4142a1031fc03dd0762b35094bb423b1b
Jordell <rjordell@gmail.com>
2023-12-15 17:18:50 -0800

    backend is up and responsive

--- r3/server/app.py
+++ r3/server/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from CargoState import Container, CargoState
 from Tree import Node, Tree
 from search import astar, backtrace, readManifest
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/solve', methods=['POST'])
+@app.route('/solve', methods=['GET'])
 def solve():
     print("hi")
+    return jsonify({"message": "hi"})
     #todo implement backend lol
 
 if __name__ == '__main__':
