387b05fbcedcec75e57b46f20c02da999308dd76
Jordell <rjordell@gmail.com>
2023-12-16 05:50:56 -0800

    made handleStartOver reset everything in cachedState and localStorage

--- r3/src/components/Home.js
+++ r3/src/components/Home.js
@@ -4,88 +4,97 @@ import handleTimestamp from './Timestamp'
 import { useNavigate } from 'react-router-dom'
 import { Alert, Button } from '@mui/material'
 
-// cached state is passed in the event of a crash. 
-// nav is used for route switching between different link paths 
+// cached state is passed in the event of a crash.
+// nav is used for route switching between different link paths
 
-function Home ({ cachedState, setCachedState }) { 
-  const nav = useNavigate() 
+function Home ({ cachedState, setCachedState }) {
+  const nav = useNavigate()
 
-  // checks if there is a file present to see if we're in the middle of an operation, if not, takes you to home page 
-  const [txtFile, setTxtFile] = useState(localStorage.getItem("manifest") ? localStorage.getItem("manifest") : null)
-  const [op, setOp] = useState(localStorage.getItem("opType") ? localStorage.getItem("opType") : "")
+  // checks if there is a file present to see if we're in the middle of an operation, if not, takes you to home page
+  const [txtFile, setTxtFile] = useState(
+    localStorage.getItem('manifest') ? localStorage.getItem('manifest') : null
+  )
+  const [op, setOp] = useState(
+    localStorage.getItem('opType') ? localStorage.getItem('opType') : ''
+  )
 
   // in the event if there is an operation in progress and we want to start over
-  const handleStartOver = () => { 
-    setCachedState({
-      ...cachedState,
-      inProgress: false, 
-      lastActivityTime: handleTimestamp()
-    })
-    //localStorage.removeItem('inProgress'); 
-    localStorage.removeItem('manifest'); 
-    localStorage.setItem('inProgress', false); 
+  const handleStartOver = () => {
+    const defaultState = {
+      inProgress: false,
+      opType: '',
+      lastActivityTime: cachedState.lastActivityTime,
+      currStep: 0,
+      totalSteps: 0,
+      lastUser: cachedState.lastUser,
+      manifest: null,
+      loadList: [],
+      offloadList: []
+    }
 
+    setCachedState(defaultState)
+
+    Object.keys(defaultState).forEach(key => {
+      localStorage.setItem(key, JSON.stringify(defaultState[key]))
+    })
+    console.log(localStorage)
   }
 
   const handleContinue = () => {
     // const activitytime = handleTimestamp()
-    console.log("continue")
-    // do something here with reloading everything from local storage 
-  
+    console.log('continue')
+    // do something here with reloading everything from local storage
   }
-  
-  // parse manifest list 
-  function parseManifestFile  (manifestTxt) {
-    const regexPattern = /\[(\d{2},\d{2})\],\s\{(\d{5})\},\s([^\r\n]+)/g;
+
+  // parse manifest list
+  function parseManifestFile (manifestTxt) {
+    const regexPattern = /\[(\d{2},\d{2})\],\s\{(\d{5})\},\s([^\r\n]+)/g
 
     // Initialize an array to store the parsed data
-    const parsedData = [];
+    const parsedData = []
 
     // Use a loop to match and extract data from the input string
-    let match;
+    let match
     while ((match = regexPattern.exec(manifestTxt)) !== null) {
-        // Extract matched groups and push them to the result array
-        const [, position, weight, name] = match;
-        parsedData.push([`[${position}]`, weight, name]);
+      // Extract matched groups and push them to the result array
+      const [, position, weight, name] = match
+      parsedData.push([`[${position}]`, weight, name])
     }
 
     //console.log(parsedData)
-    const deepCopyParsedData = JSON.stringify(parsedData);
+    const deepCopyParsedData = JSON.stringify(parsedData)
 
     setCachedState({
-      ...cachedState, 
+      ...cachedState,
       manifest: deepCopyParsedData
-    });
-    localStorage.removeItem('manifest'); 
-    localStorage.setItem('manifest', deepCopyParsedData); 
+    })
+    localStorage.removeItem('manifest')
+    localStorage.setItem('manifest', deepCopyParsedData)
 
     if (op === 'Offloading/Onloading') {
       nav('/upload-transfer')
     } else {
       nav('/ship-view')
     }
-
-
-  };
-  
+  }
 
   const handleSubmit = e => {
     // logging function goes here
     const uploadtime = handleTimestamp()
     e.preventDefault()
 
-    const fileReader = new FileReader();
-    fileReader.onload = (e) => {
-      const manifestContent = e.target.result;
+    const fileReader = new FileReader()
+    fileReader.onload = e => {
+      const manifestContent = e.target.result
       // Set the manifest content in the state
       //console.log(manifestContent);
-      localStorage.setItem('manifest', manifestContent);
-      parseManifestFile(manifestContent);
+      localStorage.setItem('manifest', manifestContent)
+      parseManifestFile(manifestContent)
     }
 
     localStorage.setItem('opType', op)
     localStorage.setItem('lastActivityTime', uploadtime)
-    fileReader.readAsText(txtFile);
+    fileReader.readAsText(txtFile)
   }
 
   const handleLogout = () => {
@@ -93,35 +102,39 @@ function Home ({ cachedState, setCachedState }) {
     localStorage.setItem('lastUser', '')
     nav('/')
   }
-  
+
   const testBackend = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/solve');
-      const data = await response.json();
+      const response = await fetch('http://127.0.0.1:5000/solve')
+      const data = await response.json()
       if (response.ok) {
-        console.log(data);
-        console.log('Backend is running and responsive!');
+        console.log(data)
+        console.log('Backend is running and responsive!')
       } else {
-        console.error('Backend is not responsive. Status:', response.status);
+        console.error('Backend is not responsive. Status:', response.status)
       }
     } catch (error) {
-      console.error('Error while trying to reach the backend:', error.message);
+      console.error('Error while trying to reach the backend:', error.message)
     }
-  };
+  }
 
   //testBackend();
 
-  // refreshes every single time CachedState is changed.  async function 
+  // refreshes every single time CachedState is changed.  async function
 
   return (
     <div>
       {cachedState.inProgress ? (
         <Alert>
-            This is currently an operation in progress. would you like to continue? 
-            <Button color="success"  onClick={handleContinue}>Yes</Button>
-            <Button color="warning" onClick={handleStartOver}>No</Button>
+          This is currently an operation in progress. would you like to
+          continue?
+          <Button color='success' onClick={handleContinue}>
+            Yes
+          </Button>
+          <Button color='warning' onClick={handleStartOver}>
+            No
+          </Button>
         </Alert>
-      
       ) : (
         <div>
           <button type='button' onClick={handleLogout}>
@@ -165,4 +178,4 @@ function Home ({ cachedState, setCachedState }) {
   )
 }
 
-export default Home;
+export default Home
