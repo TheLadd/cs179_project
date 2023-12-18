b1f6ad7fae613f0cfa5e2b3b96ed3f9f738115a0
nuharahman9 <nrahm009@ucr.edu>
2023-12-17 16:03:14 -0800

    setup for rendering animation

--- r3/src/components/DockView.js
+++ r3/src/components/DockView.js
@@ -69,12 +69,44 @@ export default function DockView ({ cachedState, setCachedState }) {
 // };
 
   
-    const [hoveredItem, setHoveredItem] = useState(null); 
     const BUFFER = "buffer"; 
     const SHIP = "ship"; 
+    // parameters: 
+    // prev/next = [row, col] - 1 indexed
+    // prevLocation/nextLocation = 0 || 1 || 2 
+    // 0 = ship, 1 - buffer, 2 - truck 
+    const showNextInstruction = (prev, prevLocation, next, nextLocation) => {
+        let prevID = ""; 
+        let nextID = ""; 
+        if (prevLocation === 0) {
+            let num = (prev[0] - 1) * 12 + prev[1]
+            prevID = "#ship-" + num.toString()
+        } else if (prevLocation === 1) {
+            let num = (prev[0] - 1) * 12 + prev[1]
+            prevID = "#buffer" + num.toString() 
+        } else { 
+            // have some fixed id for truck 
+        }
+
+        if (nextLocation === 0) {
+            let num = (next[9] - 1) * 12 + next[1]
+            nextID = "#ship-" + num.toString()
+        } else if (nextLocation === 1) {
+            let num = (next[0] - 1) * 12 + next[1]
+            nextID = "#buffer" + num.toString() 
+        } else { 
+            // have some fixed id for truck 
+        }
+
+        const prevGridItem = document.querySelector(prevID); 
+        const nextGridItem = document.querySelector(nextID); 
+
+        const prevPos = prevGridItem.getBoundingClientRect(); 
+        const nextPos = nextGridItem.getBoundingClientRect(); 
 
 
 
+    }; 
 
 
   return (
@@ -84,7 +116,9 @@ export default function DockView ({ cachedState, setCachedState }) {
             <div className='instruction-box'>
                 <h1>Step {cachedState.currStep + 1} of {cachedState.totalSteps + 1}</h1>
                 <h2 className='instruction'></h2>
-                <button>Log something</button> 
+                <button>Make Move</button>
+                <button>Skip Move</button>
+                <button>Log Something</button> 
             </div>
         </div>
   ) 
--- r3/src/components/Grid.js
+++ r3/src/components/Grid.js
@@ -1,8 +1,10 @@
 import '../css/Grid.css'; 
+import { useState } from 'react'; 
 import Container from './Container'; 
 
 export default function Grid(props) {
     let items = props.items; 
+
 
     // implemented for testing - no access to buffer yet 
     if (props.type === "buffer") {
@@ -14,6 +16,11 @@ export default function Grid(props) {
             }
         }
     }
+    
+    // create a unique identifier for it for animations to call
+    const createUniqueID = (idx) => {
+        return "buffer-" + String(idx); 
+    }
 
     // console.log(items.length); 
 
@@ -21,7 +28,8 @@ export default function Grid(props) {
         <div className={props.type}>
             <div className={props.type === "buffer" ? "buffer-header" : "grid-header"}>{props.type}</div>
             {items.map((arr, index) => 
-                <Container key={index} position={arr[0]} weight={arr[1]} name={arr[2]} /> 
+                <Container key={index} id={() => createUniqueID(index)}
+                position={arr[0]} weight={arr[1]} name={arr[2]} /> 
             )}
         </div>
     ); 
