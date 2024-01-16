ba0f9245ebb0c541c3e5b3150e1f78670e6d2eca
nuharahman9 <nrahm009@ucr.edu>
2024-01-16 10:08:54 -0800

    styling for finish page

--- src/components/Finish.js
+++ src/components/Finish.js
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'; 
 import DefaultState from './DefaultState';
-import { ButtonGroup } from '@mui/material';
+import { ButtonGroup, Button } from '@mui/material';
 import Div from "./Div"; 
 // will display the finish screen 
 const Finish = ({ cachedState, setCachedState }) => { 
@@ -14,19 +14,33 @@ const Finish = ({ cachedState, setCachedState }) => {
 
     const handleLogout = () => { 
         setCachedState(DefaultState(null)); 
-        nav('/login'); 
+        nav('/'); 
 
     }
 
     return (
         <Div sx={{
             bgcolor: 'finish.main', 
-            width: 1/2, 
+            margin: 10, 
+            padding: 30, 
+            border: 80, 
+            borderColor: 'finish.border', 
+            borderRadius: 10, 
+            fontWeight: 'bold', 
+            fontSize: 80,  
+            fontFamily: 'Raleway', 
+            color: 'finish.text', 
+            display: 'flex', 
+            textAlign: 'center', 
+            alignItems: 'center', 
+            flexDirection: 'column', 
         }}> 
-            congrats! you're done. 
-            <ButtonGroup>
-                <button onClick={handleLogout}>Logout</button>
-                <button onClick={handleNavToHome}>Home</button>
+            congrats!<br/>you're done. 
+            <ButtonGroup variant="text" aria-label="text button group" sx={{
+                color: 'finish.text', 
+            }}>
+                <Button onClick={handleLogout}>Logout</Button>
+                <Button onClick={handleNavToHome}>Home</Button>
             </ButtonGroup> 
         </Div> 
     ); 
